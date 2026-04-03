18af8dd69ceecebb7f6ee5e6a2c0ee4c23aa29dd
Daniil Sitdikov <hello.boriskas@gmail.com>
2026-04-03 07:25:39 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,19 +13,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close()
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			continue
-		}
-		go handleConnection(conn)
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
 	}
-}
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	conn.Write([]byte("+PONG\r\n"))
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read buffer")
+		os.Exit(1)
+	}
+	str := string(buf[:n])
+	for range strings.Count(str, "PING") {
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
