c80290fd24908fdc78390038276d22f8838cc7a4
Daniil Sitdikov <dsitdikov@192.168.1.136>
2026-05-09 16:01:33 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
+	"strconv"
+
 	"github.com/codecrafters-io/redis-starter-go/internal/server"
 	"github.com/codecrafters-io/redis-starter-go/internal/storage"
 )
 
 func main() {
+	portStr := flag.String("port", "6379", "port to listen on")
+	flag.Parse()
 	srv := server.New(storage.New())
-	srv.Run(6379)
+	port, err := strconv.ParseInt(*portStr, 10, 32)
+	if err != nil {
+		panic("ERR invalid port value")
+	}
+	srv.Run(int(port))
 }
